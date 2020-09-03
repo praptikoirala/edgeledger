317f07fcf5d593efe3f5cccdbeaf033d0907c79b
Prapti Koirala <praptikoirala6@gmail.com>
2020-09-03 12:37:30 +0545

    smooth scrolling is complete

--- main.js
+++ main.js
@@ -10,3 +10,30 @@ function initMap() {
 //The marker,positioned at location
    const marker=new google.maps.Marker({ position: loc, map: map});
  }
+
+//Navbar opacity
+window.addEventListener('scroll', function() {
+    if(window.scrollY > 150){
+        document.querySelector('.navbar').style.opacity = 0.8;
+   }else{
+       document.querySelector('.navbar').style.opacity = 1;
+   }
+});
+
+ // Smooth Scrolling
+ $('.navbar a, .btn').on('click', function(envent) {
+     if(this.hash !== '') {
+         event.preventDefault();
+
+         const hash = this.hash;
+
+        $('html, body').animate(
+            {
+                scrollTop: $(hash).offset().top -63
+            },
+            800
+        )
+     }
+ });
+
+
